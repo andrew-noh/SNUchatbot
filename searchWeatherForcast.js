047ab3082ae465e32a9933cd7f6cf53947ebe3e8
Hakyung Noh <hakyung1992@gmail.com>
2018-06-15 18:02:42 +0900

    Modify weather forcast code
    보여주는 일기예보 수 조정하는 코드 추가
    
    (소스 데이터: 24개 array - 오늘 포함 향후 5일, 3시간 단위)
    
    시작 인덱스, 종료 인덱스, step 지정 가능

--- searchWeatherForcast.js
+++ searchWeatherForcast.js
@@ -9,10 +9,10 @@ var requestForecastURL = url_forecast + "&lang=kr" + appId;
 // English version
 //var requestForecastURL = url_forecast + appId;
 
-
 function getDateToday() {
   var d = new Date();
-  var d2 = new Date(d.getTime() + 32400000);
+  //var d2 = new Date(d.getTime() + 32400000);
+  var d2 = new Date(d.getTime() + 0);
   var day = ("0" + d2.getDate()).slice(-2);
   var month = ("0" + (d.getMonth() + 1)).slice(-2);
   var year = d.getFullYear();
@@ -20,42 +20,46 @@ function getDateToday() {
 }
 
 function compareDays(source, target) {
-  if (source == target) {
+  var source_day = parseInt(source.split("-")[2]);
+  var target_day = parseInt(target.split("-")[2]);
+  var target_month = parseInt(target.split("-")[1]);
+
+  if (source_day === target_day) {
     return '오늘';
-  } else {
+  } else if (source_day + 1 === target_day) {
     return '내일';
+  } else {
+    return target_month + '월' + target_day + '일';
   };
+}
+
+function getForcasts(body_data, start_index, end_index, step) {
+  var body_parsed = JSON.parse(body_data);
+  var today = getDateToday();
+  var forcast_title = "일기예보";
+  var forcast_text = '';
+
+  for (var i = start_index; i <= end_index; i += step) {
+    var forcast_date = body_parsed.list[i].dt_txt.slice(0, 10);
+    var forcast_date_hr = body_parsed.list[i].dt_txt.slice(11, 13);
+    var forcast_descr = body_parsed.list[i].weather[0].description;
+    var forcast_temp = " (" + (body_parsed.list[i].main.temp - 273.15).toFixed(2) + "°C" + ")";
+    var day_text = compareDays(today, forcast_date);
+    var forcast =  '\n' + day_text + ' ' + forcast_date_hr + '시: ' + forcast_descr + forcast_temp;
+    forcast_text = forcast_text + forcast;
+  };
+
+  return forcast_text;
 }
 
 req_forecast({
   uri: requestForecastURL
 }, function(err, res, body) {
-  var body = JSON.parse(body);
-  var today = getDateToday();
-  var forcast_title = "일기예보";
-  var forcast_date1 = body.list[3].dt_txt.slice(0, 10);
-  var forcast_date1_hr = body.list[3].dt_txt.slice(11, 13);
+  var forcast_data = getForcasts(body, 2, 11, 1);
 
-  var forcast_date2 = body.list[7].dt_txt.slice(0, 10);
-  var forcast_date2_hr = body.list[7].dt_txt.slice(11, 13);
+  console.log(forcast_data);
 
-  var forcast_today_descr = body.list[3].weather[0].description;
-  var forcast_tomorrow_descr = body.list[7].weather[0].description;
-
-  var forcast_today_temp = " (" + (body.list[3].main.temp - 273.15).toFixed(2) + "°C" + ")";
-  var forcast_tomorrow_temp = " (" + (body.list[7].main.temp - 273.15).toFixed(2) + "°C" + ")";
-
-  var day1_text = compareDays(forcast_date1, today);
-  var day2_text = compareDays(forcast_date2, today);
-
-  var forcast_text_today = day1_text + ' ' + forcast_date1_hr + '시: ' + forcast_today_descr + forcast_today_temp;
-  var forcast_text_tomorrow = day2_text + ' ' + forcast_date2_hr + '시: ' + forcast_tomorrow_descr + forcast_tomorrow_temp;
-
-  var forcast_total = "일기예보:\n" + forcast_text_today + '\n' + forcast_text_tomorrow;
-
-  //context.session.weatherForecast = forcast_total;
-  console.log(forcast_total);
-
+  //context.session.weatherForecast = forcast_data;
   //callback();
 
 });
